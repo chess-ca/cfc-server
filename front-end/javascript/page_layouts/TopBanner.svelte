933ead85ab7cde6895942add3aa7a6de3b49bdd0
Don Parakin <don@weaddvalue.ca>
2021-11-24 13:34:39 -0500

    Fixed TopBanner link; added trailing '/' to avoid a redirect.

--- front-end/javascript/page_layouts/TopBanner.svelte
+++ front-end/javascript/page_layouts/TopBanner.svelte
@@ -5,7 +5,7 @@
 <section class="top">
  <div class="container">
   {#if is_signed_in }
-  <div class="is-pulled-right mt-2"><a class="button is-warning" href="/office/si/so">Sign out</a></div>
+  <div class="is-pulled-right mt-2"><a class="button is-warning" href="/office/si/so/">Sign out</a></div>
   {/if}
   <h1 class="has-text-weight-bold">CFC Office</h1>
  </div>
