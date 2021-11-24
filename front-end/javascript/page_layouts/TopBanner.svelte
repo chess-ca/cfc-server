e344fe979be8dcb9a6e7610d8a8930ed9768fe25
Don Parakin <don@weaddvalue.ca>
2021-11-24 14:32:48 -0500

    Fixed TopBanner so can now click to go to /office/ home page.

--- front-end/javascript/page_layouts/TopBanner.svelte
+++ front-end/javascript/page_layouts/TopBanner.svelte
@@ -1,12 +1,16 @@
 <script>
  export let is_signed_in = false;
+
+ function goHome() {
+     window.location = '/office/'
+ }
 </script>
 
 <section class="top">
  <div class="container">
   {#if is_signed_in }
-  <div class="is-pulled-right mt-2"><a class="button is-warning" href="/office/si/so/">Sign out</a></div>
+   <div class="is-pulled-right mt-2"><a class="button is-warning" href="/office/si/so/">Sign out</a></div>
   {/if}
-  <h1 class="has-text-weight-bold">CFC Office</h1>
+  <h1 class="has-text-weight-bold is-clickable" on:click={goHome}>CFC Office</h1>
  </div>
 </section>
